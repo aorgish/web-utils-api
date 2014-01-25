18e8ca830ed5c8504915176879d262d55028e525
Alex Orgish <aliaksandr_orgish@epam.com>
2014-01-25 19:50:08 +0300

    Add example of page-info ajax request

--- js/controllers.js
+++ js/controllers.js
@@ -15,28 +15,77 @@ angular.module('apiSample.controllers', [])
 
   .controller('viewCtrl', ["$scope", "$http", function ($scope, $http) {
 
-      $scope.$on("tokenChanged", function(event) {
-          getContentItems();
-      });
+      $scope.url = "";
+      initializeData();
 
-      $scope.data = { TotalCount : 0 };
-      
+      $scope.startSearch = function(event) {
+        if ($scope.url.trim()!=="")
+          getPageInfo();
+      }
 
-      function getContentItems() {
-          $http.get("http://alexorgish-test.apigee.net/v1/content-items?pageSize=100",
+      function initializeData() {
+	  $scope.data = { 
+              facebook : { cnt : 0, progress : 0 },
+              twitter  : { cnt : 0, progress : 0 },
+              googlep  : { cnt : 0, progress : 0 },
+              linkedin : { cnt : 0, progress : 0 },
+              pinterest: { cnt : 0, progress : 0 },
+              xing     : { cnt : 0, progress : 0 },
+              vkontakte: { cnt : 0, progress : 0 }
+	 };
+      }            
+
+      function getPageInfo() {
+          var url = encodeURIComponent($scope.url.trim());
+          $http.get("http://web-utils-api.herokuapp.com/page-info/"+url,
                    { headers: {
-                       //"Authorization": "Elateral-MSC " + token,
                        "Accept"       : "application/json"
                    } }
           ).success(function(data, status, headers, config) {
               console.log("success");
               console.debug(data);
-              $scope.data = data;
+              setResults(data);
           }). error(function(data, status, headers, config) {
               console.log("error!");
           });
       }
 
+      function setResults(response) {
+          initializeData();
+          if (response.facebook) 
+             $scope.data.facebook.cnt = response.facebook[0].share_count;
+          if (response.twitter) 
+             $scope.data.twitter.cnt = response.twitter.count;
+          if (response.google_plus)
+             $scope.data.googlep.cnt = parseInt(response.google_plus);
+          if (response.linkedin) 
+             $scope.data.linkedin.cnt = response.linkedin.count;
+          if (response.linkedin) 
+             $scope.data.linkedin.cnt = response.linkedin.count;
 
+          if (response.xing) 
+             $scope.data.xing.cnt = parseInt(response.xing);
+          if (response.vk_shares) 
+             $scope.data.vkontakte.cnt = parseInt(response.vk_shares);
+
+
+          calcProgress();
+      }
+
+
+      function calcProgress() {
+        var maxValue = 0;
+        var item;
+      	for(item in $scope.data) {
+           if ($scope.data[item].cnt>maxValue) maxValue = $scope.data[item].cnt;
+        }
+        
+        if (maxValue==0) return;
+
+      	for(item in $scope.data) {
+           $scope.data[item].progress = Math.round(100*$scope.data[item].cnt/maxValue);
+        }
+        
+      }
   }]);
 
